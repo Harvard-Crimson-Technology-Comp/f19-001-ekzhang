e65775b0370467e2a63274ee47efb9649649dcfd
Eric Zhang <ekzhang1@gmail.com>
2019-09-21 23:56:41 -0400

    submitting first half

--- project1/api/models.py
+++ project1/api/models.py
@@ -1,23 +1,42 @@
 from django.db import models
 
-# TODO: you'll need to fill in the models that you create here!
-
-# a class containing what happened in a purchase (who made it, when it was made, what it traded)
-class PurchaseModel(models.Model):
-    class Meta:
-        app_label = 'api'
-
-# the class containing your user information (including their api token and password, etc)
-class UserModel(models.Model):
-    class Meta:
-        app_label = 'api'
 
 # the class containing a portfolio of stocks
 class PortfolioModel(models.Model):
     class Meta:
         app_label = 'api'
+    cash = models.FloatField()
+
+
+# the class containing your user information (including their api token and password, etc)
+class UserModel(models.Model):
+    class Meta:
+        app_label = 'api'
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    # how long are our API tokens?
+    api_token = models.CharField(max_length=256)
+    # how long is this? I'll assume hex, 256 bit hash
+    password = models.CharField(max_length=64)
+    username = models.CharField(max_length=30)
+    portfolio = models.OneToOneField(PortfolioModel, on_delete=models.CASCADE)
+
+
+# a class containing what happened in a purchase (who made it, when it was made, what it traded)
+class PurchaseModel(models.Model):
+    class Meta:
+        app_label = 'api'
+    timestamp = models.DateTimeField()
+    user = models.ForeignKey(UserModel, on_delete=models.CASCADE)
+    symbol = models.CharField(max_length=20)
+    price = models.FloatField()
+    quantity = models.IntegerField()
+
 
 # a class containing a simple set of data on a stock within a portfolio
 class StockModel(models.Model):
     class Meta:
         app_label = 'api'
+    portfolio = models.ForeignKey(PortfolioModel, on_delete=models.CASCADE)
+    symbol = models.CharField(max_length=20)
+    quantity = models.IntegerField()
--- project1/api/schema.py
+++ project1/api/schema.py
@@ -1,1 +1,42 @@
-# TODO! You'll need to add the GraphQL schema for your project here!
+import graphene
+from graphene_django import DjangoObjectType
+
+from .models import PurchaseModel, UserModel, PortfolioModel, StockModel
+
+class Purchase(DjangoObjectType):
+    class Meta:
+        model = PurchaseModel
+
+
+class User(DjangoObjectType):
+    class Meta:
+        model = UserModel
+
+
+class Portfolio(DjangoObjectType):
+    class Meta:
+        model = PortfolioModel
+
+
+class Stock(DjangoObjectType):
+    class Meta:
+        model = StockModel
+
+
+class Query(graphene.ObjectType):
+    purchases = graphene.List(Purchase)
+    users = graphene.List(User)
+    portfolios = graphene.List(Portfolio)
+    stocks = graphene.List(Stock)
+
+    def resolve_purchases(self, info):
+        return PurchaseModel.objects.all()
+
+    def resolve_users(self, info):
+        return UserModel.objects.all()
+
+    def resolve_portfolios(self, info):
+        return PortfolioModel.objects.all()
+
+    def resolve_stocks(self, info):
+        return StockModel.objects.all()
